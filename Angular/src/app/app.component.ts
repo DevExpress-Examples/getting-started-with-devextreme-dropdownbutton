85f4aa49791f3fbe20738ebd63e7966b253e8150
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 11:01:23 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -4,9 +4,10 @@ import { DxDropDownButtonTypes } from 'devextreme-angular/ui/drop-down-button';
 import { ActionItem, DropDownOptions } from './app.types';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   actions: ActionItem[] = [
